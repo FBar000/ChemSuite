a4feb4c63e63374725b806d72be893a3b81ec5b8
FBar000 <barrera.federico@outlook.com>
2023-03-29 00:25:06 -0400

    section done

--- acidsbases.py
+++ acidsbases.py
@@ -5,7 +5,7 @@ This module contains scripts for calculating pH/OH values, acid/base constants, 
 import getMoles
 from decimal import Decimal
 
-def calcTitrantMass(pH, Ka, V, M, X):
+def calcBaseTitrantMass(pH, Ka, V, M, X):
     """
     Returns the mass of the base titrant needed to achieve a given pH from volume of an acid solution with a given molarity.
 
@@ -25,23 +25,54 @@ def calcTitrantMass(pH, Ka, V, M, X):
     molar_mass = Decimal(getMoles.getMolarMass(X))
     return cH * M * V * molar_mass * Ka
 
+def calcAcidTitrantMass(pH, Kb, V, M, X):
+    """
+    Returns the mass of the acid titrant needed to achieve a given pH from volume of an acid solution with a given molarity.
+
+    This assumes that adding a substance does not change the volume of the solution.
+
+    Args: 
+        pH  (Decimal):  The target pH
+        Kb  (Decimal):  The base's equilibrium constant 
+        M   (Decimal):  The base's molarity. (Molal)
+        V   (Decimal):  The base's volume.   (Liters)
+        X   (string):   The acid's chemical formula.
+
+    Return:
+        m (Decimal):    The mass of acid required.
+    """
+    Ka = Decimal(10**(-14)) / Kb
+    molar_mass = Decimal(getMoles.getMolarMass(X))
+    cH = 10**(pH)
+    return molar_mass * M * V / (cH * Ka) 
+
+
+
+
 if __name__ == '__main__':
 
     givens = {
-        "pH": 5.31,
-        "Ka": 1.8*10**(-5),
-        "V": 0.125,
-        "M": 1.3,
-        "X": "NaCH3CO2"
+        "pH": 3.66,
+        "K": 4.5*10**(-4),
+        "V": 0.3,
+        "M": 0.7,
+        "X": "KNO2"
     }
 
     for i in givens:
         if type(givens[i]) == float:
             givens[i] = Decimal(givens[i])
 
-    print(calcTitrantMass(
+    print(calcAcidTitrantMass(
                         givens["pH"],
-                        givens["Ka"],
+                        givens["K"],
+                        givens["V"],
+                        givens["M"],
+                        givens["X"]))
+    
+    print(calcBaseTitrantMass(
+                        givens["pH"],
+                        givens["K"],
                         givens["V"],
                         givens["M"],
                         givens["X"]))
